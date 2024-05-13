8366691eb1a437c2d91500603569c1554c313a90
lushaoqing <lushaoqing@xunlei.com>
2024-05-13 20:16:55 +0800

    Init

---
+++ shared.d.ts
@@ -0,0 +1,13 @@
+import { Swiper } from './index.d';
+
+export interface CSSSelector extends String {}
+
+export type SwiperModule = (options: {
+  params: Swiper['params'];
+  swiper: Swiper;
+  extendParams: (obj: { [name: string]: any }) => void;
+  on: Swiper['on'];
+  once: Swiper['once'];
+  off: Swiper['off'];
+  emit: Swiper['emit'];
+}) => void;
